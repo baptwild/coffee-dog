d77c8f9717709c656089a36c58cfeba37369e290
Nicolas Osborne <nho.osborne@gmail.com>
2025-06-24 23:11:15 +0200

    add prices/tarifs static page

---
+++ src/Controller/PricesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PricesController extends AbstractController
+{
+   #[Route('/tarifs', name: 'prices')]
+   public function index(): Response
+   {
+      return $this->render('pages/prices/index.html.twig', [
+         'controller_name' => 'PricesController',
+      ]);
+   }
+}
