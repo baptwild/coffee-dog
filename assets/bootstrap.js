650e49e917e24a8e34632c256423e685046d53aa
Nicolas Osborne <nho.osborne@gmail.com>
2025-06-30 16:14:25 +0200

    add mobile menu and javascript interactions

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,10 +1,14 @@
-import { startStimulusApp } from '@symfony/stimulus-bridge';
+import { startStimulusApp } from '@symfony/stimulus-bridge'
+import MenuController from './controllers/menu_controller'
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
-export const app = startStimulusApp(require.context(
+export const app = startStimulusApp(
+  require.context(
     '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
     true,
     /\.[jt]sx?$/
-));
+  )
+)
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
+app.register('menu', MenuController)
