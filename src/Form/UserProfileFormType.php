5b036e9d0536b0c9a35ed120ae26122b6b843140
Nicolas Osborne <nho.osborne@gmail.com>
2025-07-13 15:54:17 +0200

    add style to Register, Login and Profile forms - Update paths for login, logout and register links

--- src/Form/UserProfileFormType.php
+++ src/Form/UserProfileFormType.php
@@ -14,53 +14,53 @@ class UserProfileFormType extends AbstractType
     {
         $builder
             ->add('firstName', TextType::class, [
-                'label' => 'Prénom',
+                'label' => 'Prénom :',
                 'attr' => [
                     'readonly' => true,
                 ],
             ])
             ->add('lastName', TextType::class, [
-                'label' => 'Nom',
+                'label' => 'Nom :',
                 'attr' => [
                     'readonly' => true,
                 ],
             ])
             ->add('email', TextType::class, [
-                'label' => 'Email',
+                'label' => 'Email :',
                 'attr' => [
                     'readonly' => true,
                 ],
             ])
             ->add('phoneNumber', TextType::class, [
-                'label' => 'Numéro de téléphone',
+                'label' => 'Numéro de téléphone :',
                 'required' => false,
                 'attr' => [
                     'readonly' => true,
                 ],
             ])
             ->add('address', TextType::class, [
-                'label' => 'Adresse',
+                'label' => 'Adresse :',
                 'required' => false,
                 'attr' => [
                     'readonly' => true,
                 ],
             ])
             ->add('city', TextType::class, [
-                'label' => 'Ville',
+                'label' => 'Ville :',
                 'required' => false,
                 'attr' => [
                     'readonly' => true,
                 ],
             ])
             ->add('zipCode', TextType::class, [
-                'label' => 'Code postal',
+                'label' => 'Code postal :',
                 'required' => false,
                 'attr' => [
                     'readonly' => true,
                 ],
             ])
             ->add('complementaryAddress', TextType::class, [
-                'label' => 'Complément d\'adresse',
+                'label' => 'Complément d\'adresse :',
                 'required' => false,
                 'attr' => [
                     'readonly' => true,
